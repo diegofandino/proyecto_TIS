6cf2a7055bb429837912735a009cdc4c1460ec7a
Diego Fandiño <diegofandino.u@gmail.com>
2020-10-11 11:53:44 -0500

    Se realizan los componentes del sidebar, header y contenido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContenidoComponent } from './components/contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule,

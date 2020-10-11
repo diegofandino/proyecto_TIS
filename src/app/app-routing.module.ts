e1aec13e90e66935573e5cfe2b34c541eadcd6fd
Diego Fandiño <diegofandino.u@gmail.com>
2020-10-11 12:18:36 -0500

    Se agregan las imagenes para empezar a trabajar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContenidoComponent } from './components/contenido/contenido.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContenidoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

1535a7885f3567c4bd1934638cef0b70a39b8c7c
Diego Fandiño <diegofandino.u@gmail.com>
2020-10-17 17:38:11 -0500

    Se esta empezando a ajustar y realizar la pantalla de listar o consultar usuarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContenidoComponent } from './components/contenido/contenido.component';
+import { UsuariosComponent } from '../app/components/usuarios/usuarios.component';
+
 
 const routes: Routes = [
   { path: '', component: ContenidoComponent },
+  { path: 'usuarios', component: UsuariosComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,17 +10,20 @@ import { environment } from '../environments/environment';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ContenidoComponent } from './components/contenido/contenido.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HeaderComponent,
-    ContenidoComponent
+    ContenidoComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     NgbModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],

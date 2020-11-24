04d1ab375a5c7a277090caef3b7bd4a0ed5dc5a6
Diego Fandiño <diegofandino.u@gmail.com>
2020-11-24 12:19:00 -0500

    Se elimina el plugin pdf para revisar otros

--- src/app/components/reportes/reportes.component.ts
+++ src/app/components/reportes/reportes.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { DashboardService } from 'src/app/services/dashboard.service';
-import jsPDF from 'jspdf'
-import 'jspdf-autotable'
 import { map } from 'rxjs/operators';
-
 
 @Component({
   selector: 'app-reportes',
@@ -25,18 +22,7 @@ export class ReportesComponent implements OnInit {
           console.log('usuario');
           this.dashboardService.listarUsuariosPDF().subscribe( (data) => {
 
-            let doc = new jsPDF('l', 'pt');
-            var col = ['Nombre', 'Apellido', 'Documento', 'Teléfono ','Correo electrónico'];
-            var rows = [];
-
-            data.forEach(element => {      
-              var temp = [element.nombre,element.apellido,element.documento,element.email,element.telefono];
-              rows.push(temp);
-      
-          });  
-            doc.autoTable(col, rows);
-            doc.save('prueba.pdf');
-
+            console.log(data);
           }); 
 
         break;
